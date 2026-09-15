bd334620eb58d1c01865cca27395e8553652e3ce
Sandra Moen <sandramoen01@gmail.com>
2026-09-15 11:41:45 +0200

    added broken objects, to identify our 'levels'

---
+++ core/src/main/java/no/sandramoen/libgdx38/actors/broken/BlueThick.java
@@ -0,0 +1,9 @@
+package no.sandramoen.libgdx38.actors.broken;
+
+public class BlueThick extends Broken{
+
+    public BlueThick() {
+        num_pieces = 4;
+        image_path += "blue_thick";
+    }
+}
---
+++ core/src/main/java/no/sandramoen/libgdx38/actors/broken/CatMug.java
@@ -0,0 +1,9 @@
+package no.sandramoen.libgdx38.actors.broken;
+
+public class CatMug extends Broken{
+
+    public CatMug() {
+        num_pieces = 4;
+        image_path += "cat_mug";
+    }
+}
